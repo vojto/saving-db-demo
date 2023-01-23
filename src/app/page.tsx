5d690805c05f4cd2f4954dbc4168165d38d62e54
Vojtech Rinik <vojto@rinik.net>
2023-01-23 21:05:45 +0100

    svaing db demo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,65 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+"use client";
 
-const inter = Inter({ subsets: ['latin'] })
+import { useEffect, useState } from "react";
+import localforage from "localforage";
+
+let data = "";
+const dataSize = 20 * 1024 * 1024; // 10mb
+const possibleChars =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+for (let i = 0; i < dataSize; i++) {
+  data += possibleChars.charAt(
+    Math.floor(Math.random() * possibleChars.length)
+  );
+}
 
 export default function Home() {
+  const [x, setX] = useState(0);
+
+  const animate = () => {
+    setX((x) => (x < 500 ? x + 1 : 0));
+
+    requestAnimationFrame(animate);
+  };
+
+  useEffect(() => {
+    requestAnimationFrame(animate);
+  }, []);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main className="p-10 bg-zinc-100 absolute inset-0">
+      <button
+        onClick={() => {
+          // const start = performance.now();
+          // while (performance.now() - start < 1000) {
+          //   // do nothing
+          // }
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
+          const key = generateRandomString();
+          localforage.setItem(key, data);
+        }}
+      >
+        Freeze
+      </button>
 
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <span
+        className="block absolute  bg-blue-600 p-4 rounded text-white"
+        style={{ left: `${x}px`, top: `${x}px` }}
+      >
+        Hello world
+      </span>
     </main>
-  )
+  );
+}
+
+function generateRandomString(): string {
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
 }
